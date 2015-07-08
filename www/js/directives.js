536b7d9b669077fb20fbd795da0aa1554b52b175
Thomas Jacquin <jacquin.thomas@gmail.com>
2015-07-08 00:46:59 -0700

    ADDED bootstrap UI date and time

--- www/js/directives.js
+++ www/js/directives.js
@@ -25,43 +25,78 @@ angular.module('ePCR.directives', [])
 .directive('myDateTimePicker', function ($ionicPopup) {
   return {
     restrict: 'E',
-    template: '<input class="my-date-time-picker" type="text" readonly="readonly" ng-model="dateModel" ng-click="popup()" placeholder="{{placeholder}}">',
+    template: '<input class="my-date-time-picker" type="text" readonly="readonly" ng-model="dateModel" ng-click="openModal()" placeholder="{{placeholder}}">',
     scope: {
       'title': '@',
       'dateModel': '=ngModel',
       'placeholder': '@'
     },
-    controller : function($scope, $filter, $ionicPopup) {
+    controller : function($scope, $filter, $ionicModal) {
       $scope.tmp = {};
-      $scope.pop = null;
-      $scope.tmp.newDate = $scope.dateModel || Date.now();
-      
-      $scope.onTimeSet = function(newDate, oldDate) {
-//        $scope.dateModel = $scope.tmp.newDate;
-        $scope.dateModel = $filter('date')($scope.tmp.newDate, 'medium');
-        $scope.pop.close();
-      };
+      $scope.myTime = new Date();
+      $scope.newDate = new Date();
+       $scope.dateOptions = {
+          formatYear: 'yy',
+          startingDay: 1
+        };
 
-      $scope.popup = function() {
-        $scope.pop = $ionicPopup.show({
-          template: '<div class="my-date-time-picker"><datetimepicker data-ng-model="tmp.newDate" data-on-set-time="onTimeSet(newDate, oldDate)"></datetimepicker></div>',
-          title: $scope.title,
+        $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+        $scope.format = $scope.formats[0];
+
+      $scope.open = function($event) {
+    $event.preventDefault();
+    $event.stopPropagation();
+
+    $scope.opened = true;
+  };
+
+        
+        $ionicModal.fromTemplateUrl('templates/shared/date-time-modal.html', {
           scope: $scope,
-          buttons: [
-            {text: 'Cancel'},
-            {
-              text: '<b>Choose</b>',
-              type: 'button-positive',
-              onTap: function(e) {
-                //$scope.$apply(function() { //error: apply already in progress
-                  $scope.dateModel = $scope.tmp.newDate;
-                  $scope.formatted_datetime = $filter('date')($scope.tmp.newDate, 'medium');
-                //});
-              }
-            } //second button
-          ] //buttons array
-        }); //ionicpopup.show
-      }; //scope.popup();
+          animation: 'slide-in-up'
+        }).then(function(modal) {
+          $scope.myTime = new Date();
+          $scope.modal = modal;
+        });
+        $scope.openModal = function() {
+          $scope.modal.show();
+        };
+        $scope.closeModal = function() {
+          $scope.modal.hide();
+        };
+        //Cleanup the modal when we're done with it!
+        $scope.$on('$destroy', function() {
+          $scope.modal.remove();
+        });
+        // Execute action on hide modal
+        $scope.$on('modal.hidden', function() {
+          // Execute action
+        });
+        // Execute action on remove modal
+        $scope.$on('modal.removed', function() {
+          // Execute action
+        });
+        
+
+//        $scope.pop = $ionicPopup.show({
+//          template: '',
+//          title: $scope.title,
+//          scope: $scope,
+//          buttons: [
+//            {text: 'Cancel'},
+//            {
+//              text: '<b>Choose</b>',
+//              type: 'button-positive',
+//              onTap: function(e) {
+//                //$scope.$apply(function() { //error: apply already in progress
+//                  $scope.dateModel = $scope.tmp.newDate;
+//                  $scope.formatted_datetime = $filter('date')($scope.tmp.newDate, 'medium');
+//                //});
+//              }
+//            } //second button
+//          ] //buttons array
+//        }); //ionicpopup.show
+//      }; //scope.popup();
     }
   };
 });
